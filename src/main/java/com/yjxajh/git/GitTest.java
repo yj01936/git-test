ad3b1572385f8369555bbb4c4f91f6e2523fb2c3
yanjing <yjxajh@sina.com>
2021-08-15 23:34:12 +0800

    second commit

--- src/main/java/com/yjxajh/git/GitTest.java
+++ src/main/java/com/yjxajh/git/GitTest.java
@@ -7,5 +7,6 @@ package com.yjxajh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello World!");
     }
 }
