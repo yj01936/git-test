d97b634cb04414620aad7dd89b8badf814ea0aca
yanjing <yjxajh@sina.com>
2021-08-15 23:25:37 +0800

    first commit

---
+++ src/main/java/com/yjxajh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yjxajh.git;
+
+/**
+ * @author 闫静
+ * @create 2021/8/15-23:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
