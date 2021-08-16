64399f53151ce46dbc01e3c5e9063a0827a7ea2e
yanjing <yjxajh@sina.com>
2021-08-16 22:02:23 +0800

    hot-fix first commit

--- src/main/java/com/yjxajh/git/GitTest.java
+++ src/main/java/com/yjxajh/git/GitTest.java
@@ -7,7 +7,7 @@ package com.yjxajh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
     }
